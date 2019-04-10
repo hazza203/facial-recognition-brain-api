3e503e1050a1f82037531a2c4e3564af5beca767
Hazza203 <s3383212@student.rmit.edu.au>
2019-04-11 00:41:23 +0800

    Changed database urls

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const profile = require('./controllers/profile')
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'harry',
-		password: 'test',
-		database: 'smart-brain'
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
 })
 
